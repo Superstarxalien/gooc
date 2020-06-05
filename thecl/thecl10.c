78dd040d26c4c6502e62bb3401b4b609afb36d41
ManDude <7569514+ManDude@users.noreply.github.com>
2020-06-05 03:37:52 +0100

    fix serious bug with immediate params being negative

--- thecl/thecl10.c
+++ thecl/thecl10.c
@@ -580,7 +580,7 @@ c1_instr_serialize(
             const char* next_format;
             uint32_t b = strtol(format + i, &next_format, 10);
             i = next_format - format;
-            val = p;
+            val = p & (0xFFFFFFFFU >> (32-b));
             if (((int64_t)p < -(1LL << b) || (int64_t)p > (1LL << b) - 1) && !ignore_error) {
                 fprintf(stderr, "%s:c1_instr_serialize: in sub %s: parameter out of bounds for instruction %d (%u bits)\n", argv0, sub->name, instr->id, b);
             }
@@ -1091,7 +1091,7 @@ c2_instr_serialize(
             const char* next_format;
             uint32_t b = strtol(format + i, &next_format, 10);
             i = next_format - format;
-            val = p;
+            val = p & (0xFFFFFFFFU >> (32-b));
             if (((int64_t)p < -(1LL << b) || (int64_t)p > (1LL << b) - 1) && !ignore_error) {
                 fprintf(stderr, "%s:c2_instr_serialize: in sub %s: parameter out of bounds for instruction %d (%u bits)\n", argv0, sub->name, instr->id, b);
             }
