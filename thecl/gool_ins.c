51169c53b0988ad09113f9ba1c80a226cfefe717
ManDude <7569514+ManDude@users.noreply.github.com>
2020-08-15 04:24:52 +0100

    gool_ins: add "debugfunc" (does nothing)

--- thecl/gool_ins.c
+++ thecl/gool_ins.c
@@ -869,6 +869,25 @@ c1_gool_ins_movetolist_params(
 }
 
 static list_t*
+c1_gool_ins_debugfunc_params(
+    list_t* params,
+    int argc)
+{
+    thecl_param_t* param;
+    size_t c = list_count(params);
+    if (c == 1) {
+        list_append_new(params, param_val_new(5));
+        list_append_new(params, param_val_new(3));
+        list_append_new(params, param_val_new(12));
+    }
+    else {
+        fprintf(stderr, "%s:%s:debugfunc: wrong number of arguments (expected 1, got %zu)\n", argv0, current_input, c);
+        return NULL;
+    }
+    return params;
+}
+
+static list_t*
 c1_gool_ins_gamefunc4_params(
     list_t* params,
     int argc)
@@ -1200,6 +1219,7 @@ c1_gool_ins[] = {
     { "savecheckpoint",             28, 0, 0, 0, -1, c1_gool_ins_savecheckpoint_params },
     { "loadcheckpoint",             28, 0, 0, 0, -1, c1_gool_ins_loadcheckpoint_params },
     { "movetolist",                 28, 0, 0, 0, -1, c1_gool_ins_movetolist_params },
+    { "debugfunc",                  28, 0, 0, 0, -1, c1_gool_ins_debugfunc_params },
     { "gamefunc4",                  28, 0, 0, 0, -1, c1_gool_ins_gamefunc4_params },
     { "soundstop",                  28, 0, 0, 0, -1, c1_gool_ins_soundstop_params },
     { "seqplay",                    28, 0, 0, 0, -1, c1_gool_ins_seqplay_params },
@@ -1417,6 +1437,7 @@ c2_gool_ins[] = {
     { "savecheckpoint",             28, 0, 0, 0, -1, c2_gool_ins_savecheckpoint_params },
     { "loadcheckpoint",             28, 0, 0, 0, -1, c2_gool_ins_loadcheckpoint_params },
     { "movetolist",                 28, 0, 0, 0, -1, c1_gool_ins_movetolist_params },
+    { "debugfunc",                  28, 0, 0, 0, -1, c1_gool_ins_debugfunc_params },
     { "gamefunc4",                  28, 0, 0, 0, -1, c1_gool_ins_gamefunc4_params },
     { "soundstop",                  28, 0, 0, 0, -1, c1_gool_ins_soundstop_params },
     { "seqplay",                    28, 0, 0, 0, -1, c1_gool_ins_seqplay_params },
