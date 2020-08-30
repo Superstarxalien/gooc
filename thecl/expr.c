1a1fb5c94d4589d2c059d779bb269dfdf48ed7f0
ManDude <7569514+ManDude@users.noreply.github.com>
2020-08-30 23:43:27 +0100

    update some comments

--- thecl/expr.c
+++ thecl/expr.c
@@ -43,63 +43,58 @@ global_expressions[] = {
     { GLOAD,     31, 0, 0, 0, 0 }, /* p0 */
     { CLOAD,     35, 0, 0, 0, 0 }, /* p0 */
     { PLOAD,     38, 0, 0, 0, 0 }, /* &p0 */
-    { ASSIGN,    17, 1, 0, 0, 0 }, /* p0 = s0 */
-    { PASSIGN,   20, 2, 0, 0, 1 }, /* p0 = &s0 */
-    { GASSIGN,   32, 1, 0, 0, 0 }, /* p0 = s0 */
-    { CASSIGN,   36, 1, 0, 0, 0 }, /* p0 = s0 */
+    { ASSIGN,    17, 1, 0, 0, 0 }, /* p0 = p1 */
+    { PASSIGN,   20, 2, 0, 0, 1 }, /* p0 = &p1 */
+    { GASSIGN,   32, 1, 0, 0, 0 }, /* p0 = p1 */
+    { CASSIGN,   36, 1, 0, 0, 0 }, /* p0 = p1 */
 
-    { ADD,        0, 2, 0, 1, 0 }, /* s1 + s0 */
-    { SUBTRACT,   1, 2, 0, 1, 0 }, /* s1 - s0 */
-    { MULTIPLY,   2, 2, 0, 1, 0 }, /* s1 * s0 */
-    { DIVIDE,     3, 2, 0, 1, 0 }, /* s1 / s0 */
-    { EQUAL,      4, 2, 0, 1, 0 }, /* s1 == s0 */
-    { INEQUAL,   -2, 2, 0, 1, 0 }, /* s1 != s0 */ /* GOOL does not have this instruction, use NOT + CEQ instead */
-    { AND,        5, 2, 0, 1, 0 }, /* s1 && s0 */
-    { OR,         6, 2, 0, 1, 0 }, /* s1 || s0 */
-    { B_AND,      7, 2, 0, 1, 0 }, /* s1 & s0 */
-    { B_OR,       8, 2, 0, 1, 0 }, /* s1 | s0 */
-    { LT,         9, 2, 0, 1, 0 }, /* s1 < s0 */
-    { LTEQ,      10, 2, 0, 1, 0 }, /* s1 <= s0 */
-    { GT,        11, 2, 0, 1, 0 }, /* s1 > s0 */
-    { GTEQ,      12, 2, 0, 1, 0 }, /* s1 >= s0 */
-    { MODULO,    13, 2, 0, 1, 0 }, /* s1 % s0 */
-    { XOR,       14, 2, 0, 1, 0 }, /* s1 ^ s0 */
-    { TEST,      15, 2, 0, 1, 0 }, /* s1 \ s0 */
-    { RAND,      16, 2, 0, 0, 0 }, /* rand(s0, s1) */
-    { NOT,       18, 2, 0, 1, 1 }, /* !s0 */
-    { LOOPSEEK,  19, 3, 1, 0, 0 }, /* loopseek(s0, s1, s2) */
-    { LSHIFT,    21, 2, 0, 1, 0 }, /* s1 << s0 */
-    { RSHIFT,    -1, 2, 0, 1, 0 }, /* s1 >> s0 */
-    { B_NOT,     23, 2, 0, 1, 1 }, /* ~s0 */
-    { ABS,       25, 2, 0, 1, 1 }, /* abs(s0) */
-    { PAD,       26, 5, 0, 0, 0 }, /* pad(s0, s1, s2, s3, s4) */
-    { SPD,       27, 2, 0, 0, 0 }, /* spd(s0, s1) */
-    { MISC,      28, 4, 0, 0, 0 }, /* misc(s0, s1, s2, s3) */
-    { PSIN,      29, 2, 0, 1, 0 }, /* sin(s0, s1) */
-    { TIME,      30, 2, 0, 0, 0 }, /* time(s0, s1) */
+    { ADD,        0, 2, 0, 1, 0 }, /* p0 + p1 */
+    { SUBTRACT,   1, 2, 0, 1, 0 }, /* p0 - p1 */
+    { MULTIPLY,   2, 2, 0, 1, 0 }, /* p0 * p1 */
+    { DIVIDE,     3, 2, 0, 1, 0 }, /* p0 / p1 */
+    { EQUAL,      4, 2, 0, 1, 0 }, /* p0 == p1 */
+    { INEQUAL,   -2, 2, 0, 1, 0 }, /* p0 != p1 */ /* GOOL does not have this instruction, uses NOT + CEQ instead */
+    { AND,        5, 2, 0, 1, 0 }, /* p0 && p1 */
+    { OR,         6, 2, 0, 1, 0 }, /* p0 || p1 */
+    { B_AND,      7, 2, 0, 1, 0 }, /* p0 & p1 */
+    { B_OR,       8, 2, 0, 1, 0 }, /* p0 | p1 */
+    { LT,         9, 2, 0, 1, 0 }, /* p0 < p1 */
+    { LTEQ,      10, 2, 0, 1, 0 }, /* p0 <= p1 */
+    { GT,        11, 2, 0, 1, 0 }, /* p0 > p1 */
+    { GTEQ,      12, 2, 0, 1, 0 }, /* p0 >= p1 */
+    { MODULO,    13, 2, 0, 1, 0 }, /* p0 % p1 */
+    { XOR,       14, 2, 0, 1, 0 }, /* p0 ^ p1 */
+    { TEST,      15, 2, 0, 1, 0 }, /* p0 \ p1 */
+    { RAND,      16, 2, 0, 0, 0 }, /* rand(p0, p1) */
+    { NOT,       18, 2, 0, 1, 1 }, /* !p1 */
+    { LOOPSEEK,  19, 3, 1, 0, 0 }, /* loopseek(p0, p1, p2) */
+    { LSHIFT,    21, 2, 0, 1, 0 }, /* p0 << p1 */
+    { RSHIFT,    -1, 2, 0, 1, 0 }, /* p0 >> p1 */
+    { B_NOT,     23, 2, 0, 1, 1 }, /* ~p1 */
+    { ABS,       25, 2, 0, 1, 1 }, /* abs(p1) */
+    { PAD,       26, 5, 0, 0, 0 }, /* pad(p0, p1, p2, p3, p4) */
+    { SPD,       27, 2, 0, 0, 0 }, /* spd(p0, p1) */
+    { MISC,      28, 4, 0, 0, 0 }, /* misc(p0, p1, p2, p3) */
+    { PSIN,      29, 2, 0, 1, 0 }, /* sin(p0, p1) */
+    { TIME,      30, 2, 0, 0, 0 }, /* time(p0, p1) */
     { GETGLOBAL, 31, 1, 0, 0, 0 }, /* getglobal(p0) */
-    { DEGDIST,   33, 2, 0, 0, 0 }, /* degdist(s0, s1) */
-    { SEEK,      34, 3, 1, 0, 0 }, /* seek(s0, s1, s2) */
-    { GETCOLOR,  35, 2, 0, 0, 0 }, /* getcolor(s0, s1) */
-    { DEGSEEK,   37, 3, 1, 0, 0 }, /* degseek(s0, s1, s2) */
-    { GETANIM,   39, 2, 0, 0, 1 }, /* getanim(s0) */
+    { DEGDIST,   33, 2, 0, 0, 0 }, /* degdist(p0, p1) */
+    { SEEK,      34, 3, 1, 0, 0 }, /* seek(p0, p1, p2) */
+    { GETCOLOR,  35, 2, 0, 0, 0 }, /* getcolor(p0, p1) */
+    { DEGSEEK,   37, 3, 1, 0, 0 }, /* degseek(p0, p1, p2) */
+    { GETANIM,   39, 2, 0, 0, 1 }, /* getanim(p1) */
     { 0,          0, 0, 0, 0, 0 }
 };
 
 static const expr_t
 c1_expressions[] = {
-    /* The program checks against the number of params, as well as the
-     * requested stack depth, and does the replacements. */
-    /* p0 is the first param, p1 the second ... */
-    /* s0 is the previous instruction, s1 the one previous to s0 ... */
-
-    /*SYM        ID     P  A SP2 O  U */
+    /*SYM        ID  A SP2 O  U */
     { CALL,    0x86, 0, 0, 0, 0 }, /* return */
     { RETURN,  0x82, 0, 0, 0, 0 }, /* return */
 
     { GOTO,    0x82, 0, 0, 0, 0 }, /* goto p0 */
-    { UNLESS,  0x82, 1, 0, 0, 0 }, /* unless (s0) goto p0 */
-    { IF,      0x82, 1, 0, 0, 0 }, /* if (s0) goto p0 */
+    { UNLESS,  0x82, 1, 0, 0, 0 }, /* unless (p1) goto p0 */
+    { IF,      0x82, 1, 0, 0, 0 }, /* if (p1) goto p0 */
 
     { EVHA,    0x89, 5, 0, 0, 0 }, /* accept event */
     { EVHR,    0x8A, 5, 0, 0, 0 }, /* reject event */
@@ -109,26 +104,21 @@ c1_expressions[] = {
 
 static const expr_t
 c2_expressions[] = {
-    /* The program checks against the number of params, as well as the
-     * requested stack depth, and does the replacements. */
-    /* p0 is the first param, p1 the second ... */
-    /* s0 is the previous instruction, s1 the one previous to s0 ... */
-
     /*SYM        ID  A SP2 O  U */
     { CALL,      59, 0, 0, 0, 0 }, /* return */
     { RETURN,    49, 0, 0, 0, 0 }, /* return */
 
     { GOTO,      50, 0, 0, 0, 0 }, /* goto p0 */
-    { IF,        51, 1, 0, 0, 0 }, /* if (s0) goto p0 */
-    { UNLESS,    52, 1, 0, 0, 0 }, /* unless (s0) goto p0 */
+    { IF,        51, 1, 0, 0, 0 }, /* if (p1) goto p0 */
+    { UNLESS,    52, 1, 0, 0, 0 }, /* unless (p1) goto p0 */
 
-    { MOVC,      24, 3, 0, 0, 0 }, /* getins(s0) */
-    { FROW,      40, 2, 0, 0, 0 }, /* fieldrow(s0, s1) */
-    { FVAL,      41, 2, 0, 0, 1 }, /* fieldval(s0) */
-    { ARRL,      42, 2, 0, 0, 0 }, /* s0[s1] */
-    { SIN,       43, 2, 0, 1, 1 }, /* sin(s0) */
-    { COS,       44, 2, 0, 1, 1 }, /* cos(s0) */
-    { ATAN,      45, 2, 0, 0, 0 }, /* atan(s1, s0) */
+    { MOVC,      24, 3, 0, 0, 0 }, /* getins(p1) */
+    { FROW,      40, 2, 0, 0, 0 }, /* fieldrow(p1, p0) */
+    { FVAL,      41, 2, 0, 0, 1 }, /* fieldval(p1) */
+    { ARRL,      42, 2, 0, 0, 0 }, /* p1[p0] */
+    { SIN,       43, 2, 0, 1, 1 }, /* sin(p1) */
+    { COS,       44, 2, 0, 1, 1 }, /* cos(p1) */
+    { ATAN,      45, 2, 0, 0, 0 }, /* atan(p0, p1) */
 
     { EVHA,      62, 5, 0, 0, 0 }, /* accept event */
     { EVHR,      63, 5, 0, 0, 0 }, /* reject event */
